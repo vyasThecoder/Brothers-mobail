db9b0d8af4ad62001c79b41f73b831d0d7799322
vyasThecoder <abhiv8111@gmail.com>
2025-09-07 21:34:44 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+// let heroImg = document.querySelector(".hero-img");
+
+// let heroImgArr = [
+//   "/images/3405349.jpg",
+//   "/images/hero-image.png",
+//   "/images/hero-img1.jpg",
+// ];
+
+// let index = 0;
+
+// console.log(heroImg);
+
+// setInterval(() => {
+//   heroImg.src = heroImgArr[index];
+//   if (index >= heroImgArr.length - 1) {
+//     index = 0;
+//   }
+//   index++;
+// }, 3000);
